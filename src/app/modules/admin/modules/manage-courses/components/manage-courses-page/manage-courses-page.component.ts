33c44e254d28d8dac1d895ae76de8ab643931866
TArch64 <tarasturchenko1551@gmail.com>
2021-09-06 19:44:05 +0300

    update courses list after season change

--- src/app/modules/admin/modules/manage-courses/components/manage-courses-page/manage-courses-page.component.ts
+++ src/app/modules/admin/modules/manage-courses/components/manage-courses-page/manage-courses-page.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {Observable} from "rxjs";
 import {ManageCoursesFacade} from "../../manage-courses.facade";
 import {map} from "rxjs/operators";
 import {MatDialog} from "@angular/material/dialog";
 import {AddCourseDialogComponent} from "../add-course-dialog";
 import {DialogSizes} from "@common/dialog";
+import {Disposable} from "@common/core";
 
 @Component({
   selector: 'app-manage-courses-page',
   templateUrl: './manage-courses-page.component.html',
   styleUrls: ['./manage-courses-page.component.css']
 })
-export class ManageCoursesPageComponent {
+export class ManageCoursesPageComponent implements OnDestroy {
+    private readonly disposable = new Disposable();
     public readonly hasCourses$: Observable<boolean> = this.createHasCoursesStream();
 
     constructor(
         private readonly facade: ManageCoursesFacade,
         private readonly matDialog: MatDialog
-    ) {}
+    ) {
+        this.attachRefreshListener();
+    }
+
+    ngOnDestroy() {
+        this.disposable.dispose();
+    }
 
     private createHasCoursesStream(): Observable<boolean> {
         return this.facade.courses$.pipe(
@@ -27,5 +35,9 @@ export class ManageCoursesPageComponent {
 
     public addCourse(): void {
         this.matDialog.open(AddCourseDialogComponent, {width: DialogSizes.MD});
+    }
+
+    private attachRefreshListener(): void {
+        this.disposable.subscribeTo(this.facade.activeSeasonChange$, () => this.facade.loadState().subscribe())
     }
 }
--- src/app/modules/admin/modules/manage-courses/manage-courses.facade.ts
+++ src/app/modules/admin/modules/manage-courses/manage-courses.facade.ts
@@ -1,13 +1,19 @@
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
 import {CoursesRepositoryService} from "./services";
-import {mapTo} from "rxjs/operators";
+import {mapTo, skip} from "rxjs/operators";
 import {Course} from "@common/course";
 import {IAddCourseOptions} from "./entities";
+import {CommonSeasonsService, Season} from "@common/season";
+import {captureExistsValues} from "@common/core";
 
 @Injectable()
 export class ManageCoursesFacade {
-    constructor(private readonly repository: CoursesRepositoryService) {}
+
+    constructor(
+        private readonly repository: CoursesRepositoryService,
+        private readonly seasonsService: CommonSeasonsService
+    ) {}
 
     public loadState(): Observable<null> {
         return this.repository.loadCourses().pipe(mapTo(null));
@@ -15,6 +21,10 @@ export class ManageCoursesFacade {
 
     public get courses$(): Observable<Course[]> {
         return this.repository.courses$;
+    }
+
+    public get activeSeasonChange$(): Observable<Season> {
+        return this.seasonsService.activeSeason$.pipe(captureExistsValues, skip(1))
     }
 
     public addCourse(options: IAddCourseOptions): Observable<Course> {
