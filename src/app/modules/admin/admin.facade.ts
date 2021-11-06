c8d2f22b1583fe5f682124b88a15efe430d317a4
TArch64 <tarasturchenko1551@gmail.com>
2021-11-06 19:09:47 +0200

    Revert "add tests for auth guards"
    This reverts commit 264d11b8aa68ff55143902aef95255e8e8a72866.

--- src/app/modules/admin/admin.facade.ts
+++ src/app/modules/admin/admin.facade.ts
@@ -1,27 +1,12 @@
 import {Injectable} from "@angular/core";
-import {CommonSeasonsService, Season} from "@common/season";
+import {CommonSeasonsService} from "@common/season";
 import {map} from "rxjs/operators";
-import {Observable, of} from "rxjs";
-
-export interface IAdminFacade {
-    hasSeasons$: Observable<boolean>;
-    loadSeasons(): Observable<Season[]>;
-}
 
 @Injectable()
-export class AdminFacade implements IAdminFacade {
+export class AdminFacade {
     public readonly hasSeasons$ = this.seasonsService.seasons$.pipe(
         map(seasons => !!seasons.length)
     );
 
-    constructor(
-        private readonly seasonsService: CommonSeasonsService
-    ) {}
-
-    loadSeasons(): Observable<Season[]> {
-        if (this.seasonsService.seasonsSnapshot) {
-            return of(this.seasonsService.seasonsSnapshot);
-        }
-        return this.seasonsService.loadSeasons();
-    }
+    constructor(private readonly seasonsService: CommonSeasonsService) {}
 }
